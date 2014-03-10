ec2b4d576a93ec8bcea51b0f55ad34b5bbc7d2c2
Christopher D Canfield <cdcanfield@gmail.com>
2014-03-10 03:25:53 -0400

    Added TiledParserTest; not yet implemented

---
+++ rts-wars/rts-wars/app/integration/TiledParserTest.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "TiledParserTest.hpp"
+
+
+// Christopher D. Canfield
+// March 2014
+// TiledParserTest.cpp
+
+using namespace cdc;
+
+TiledParserTest::TiledParserTest()
+{
+}
+
+
+TiledParserTest::~TiledParserTest()
+{
+}
---
+++ rts-wars/rts-wars/app/integration/TiledParserTest.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+// Christopher D. Canfield
+// March 2014
+// TiledParserTest.hpp
+
+namespace cdc
+{
+	///<summary>Integration test for the tiled map parser.</summary>
+	class TiledParserTest
+	{
+	public:
+		TiledParserTest();
+		virtual ~TiledParserTest();
+	};
+}
