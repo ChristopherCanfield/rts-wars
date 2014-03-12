faae60ff4d9d7f74586894a89e0f36ecb29dac97
Christopher D Canfield <cdcanfield@gmail.com>
2014-03-12 02:57:18 -0400

    Added additional comments to BspTree

--- rts-wars/rts-wars/world/map/bsp/BspTree.hpp
+++ rts-wars/rts-wars/world/map/bsp/BspTree.hpp
@@ -15,8 +15,15 @@ namespace cdc
 		BspTree();
 		~BspTree();
 
-	private:
+		///<summary>Assigns entities to leaf nodes.</summary>
+		void update();
 
+	private:
+		// The leaf nodes. Each entity should be in a leaf node.
 		std::vector<std::vector<int>> leafNodes;
+
+		// Unassigned entity indexes. These will be assigned to the correct
+		// leaf node when update() is called.
+		std::vector<int> unassigned;
 	};
 }
